aed09ed55ec048c838151680efad6447902092a9
Stan <stantonmoore42@gmail.com>
2022-04-10 23:45:23 -0400

    final

--- test.js
+++ test.js
@@ -4,29 +4,35 @@ Using map, return an array of each transformer's value of its 'form' property
 */
 //result: ['Freightliner Truck','Gun', 'VW Beetle','Walkman']
 const transformersMap = () => {
-    let transformers = [
-        {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-        {
-          name: 'Megatron',
-          form: 'Gun',
-          team: 'Decepticon'
-        },
-        {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        },
-        {
-          name: 'Soundwave',
-          form: 'Walkman',
-          team: 'Decepticon'
-        }
-      ];
+  let transformers = [
+  {
+    name: 'Optimus Prime',
+    form: 'Freightliner Truck',
+    team: 'Autobot'
+  },
+  {
+    name: 'Megatron',
+    form: 'Gun',
+    team: 'Decepticon'
+  },
+  {
+    name: 'Bumblebee',
+    form: 'VW Beetle',
+    team: 'Autobot'
+  },
+  {
+    name: 'Soundwave',
+    form: 'Walkman',
+    team: 'Decepticon'
+  }];
+
+  let trans = transformers.map(trans => {
+    return trans.form
+  })
+  console.log(trans)
 }
+transformersMap()
+
 /*
 Filter Transformers
 Using filter, return an array of transformer objects that have the 'team' property of 'Autobot'
@@ -45,30 +51,32 @@ result: [ {
 
 */
 const transformersFilter = () => {
-    let transformers = [
-        {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-        {
-          name: 'Megatron',
-          form: 'Gun',
-          team: 'Decepticon'
-        },
-        {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        },
-        {
-          name: 'Soundwave',
-          form: 'Walkman',
-          team: 'Decepticon'
-        }
-      ]
-
+  let transformers = [
+  {
+    name: 'Optimus Prime',
+    form: 'Freightliner Truck',
+    team: 'Autobot'
+  },
+  {
+    name: 'Megatron',
+    form: 'Gun',
+    team: 'Decepticon'
+  },
+  {
+    name: 'Bumblebee',
+    form: 'VW Beetle',
+    team: 'Autobot'
+  },
+  {
+    name: 'Soundwave',
+    form: 'Walkman',
+    team: 'Decepticon'
+  }]
+  const filtered = transformers.filter(trans => trans.team == 'Autobot'
+  )
+  console.log(filtered)
 }
+transformersFilter()
 
 /*
 Reduce Transformers
@@ -79,28 +87,39 @@ result: {
 }
 */
 const reduceTransformers = () => {
-    let transformers = [
-        {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-        {
-          name: 'Megatron',
-          form: 'Gun',
-          team: 'Decepticon'
-        },
-        {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        },
-        {
-          name: 'Soundwave',
-          form: 'Walkman',
-          team: 'Decepticon'
-        }
-      ]
+  let transformers = [
+  {
+    name: 'Optimus Prime',
+    form: 'Freightliner Truck',
+    team: 'Autobot'
+  },
+  {
+    name: 'Megatron',
+    form: 'Gun',
+    team: 'Decepticon'
+  },
+  {
+    name: 'Bumblebee',
+    form: 'VW Beetle',
+    team: 'Autobot'
+  },
+  {
+    name: 'Soundwave',
+    form: 'Walkman',
+    team: 'Decepticon'
+  }]
+  let reduced = transformers.reduce((acc, curr) => {
+    const team = curr.team;
+    if(acc[team])
+    {
+      acc[team]++
+    }
+    else {
+      acc[team] = 1
+    }
+    return acc;
+  }, {})
+  console.log(reduced)
 }
 
 
@@ -114,8 +133,15 @@ Use filter and reduce
 
 //result: 42
 const sumPositiveElement = () => {
-    const input = [ 1, -4, 12, 0, -3, 29, -150]
+  const input = [ 1, -4, 12, 0, -3, 29, -150]
+  const pos = input.filter((input) => input >= 0)
+              .reduce((acc, cur) => 
+              {
+                return acc + cur
+              })
+  console.log('sum pos element ' + pos)
 }
+sumPositiveElement()
 
 
 
@@ -128,8 +154,24 @@ Use: reduce, sort, Math.abs
 //result: { mean: 38.5, median: 32 }
 const medianMean = () => {
     const input = [12, 46, 32, 64]
-}
+    const sorted = input.sort()
+    const middle = Math.floor(input.length / 2)
+    let median;
+    const mean = input.reduce((acc, cur) =>{
+      return ((acc += cur) / input.length)
+    })
 
+    if (input.length % 2)
+    {
+      median = input(middle)
+    }
+    else
+    {
+      median = (input[middle-1]+input[middle]) / 2
+    }
+    console.log('mean: ' + mean + ', median: ' + median)
+}
+medianMean()
 
 /*
 Get name initials
@@ -138,9 +180,13 @@ Use .map , .split , .join
 */
 //result: 'GRRM'
 const nameInitials = () => {
-    const input = 'George Raymond Richard Martin';
+    const input = 'George Raymond Richard Martin'
+    .split(' ')
+    .map(n => n[0])
+    .join('')
+    console.log(input)
 }
-
+nameInitials()
 /*
 Age difference from the youngest and oldest
 Find the difference in age between the oldest and youngest family members, and return their respective ages and the age difference.
@@ -149,27 +195,42 @@ Use .map , Math.min , Math.max
 
 //Result: [13, 67, 54]
 const ageDifference = () => {
-    const input = [
-        {
-          name: 'John',
-          age: 13
-        },
-        {
-          name: 'Mark',
-          age: 56,
-        },
-        {
-          name: 'Rachel',
-          age: 45,
-        },
-        {
-          name: 'Nate',
-          age: 67,
-        },
-        {
-          name: 'Jeniffer',
-          age: 65,
-        }
-      ];
-}
+  const output = [];
+  const input = [
+  {
+    name: 'John',
+    age: 13
+  },
+  {
+    name: 'Mark',
+    age: 56,
+  },
+  {
+    name: 'Rachel',
+    age: 45,
+  },
+  {
+    name: 'Nate',
+    age: 67,
+  },
+  {
+    name: 'Jeniffer',
+    age: 65,
+  }];
+  const min = Math.min.apply(Math, 
+    input.map(info=>{
+      return (info.age)
+  }))
+  const max = Math.max.apply(Math, 
+    input.map(info=>{
+      return (info.age)
+  }))
+  const diff = max - min
 
+  output.push(min)
+  output.push(max)
+  output.push(diff)
+
+  console.log(output)
+}
+ageDifference()
